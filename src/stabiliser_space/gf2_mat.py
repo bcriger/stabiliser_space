7efd076aa2629f4d52907a58fb61fbe3ab8bc238
Ben Criger <BCriger@GMail.com>
2017-02-08 16:06:52 +0100

    First non-trivial commit, meas. results are a pain to get. Have yet to test.

---
+++ src/stabiliser_space/gf2_mat.py
@@ -0,0 +1,85 @@
+"""
+Copy-paste from Ed Scheinerman's SimpleGF2.jl:
+https://github.com/scheinerman/SimpleGF2.jl
+"""
+from numpy import int_, shape, zeros, identity, hstack
+from numpy.linalg import det
+from numpy.random import randint
+
+def swap_rows(mat, r0, r1):
+    temp_row = mat[r0,:].copy()
+    mat[r0,:] = mat[r1,:]
+    mat[r1,:] = temp_row
+    pass # subroutine what a treat
+
+def add_row_to_row(mat, r_add, r_to):
+    mat[r_to, :] += mat[r_add, :]
+    mat[r_to, :] %= 2
+    pass # subroutine so sweet
+
+def rref(mat):
+    """
+    Places a matrix into reduced-row echelon form using row-swapping
+    and adding, where the addition is performed mod 2.
+    """
+    r, c = shape(mat)
+    s = 0
+    for x in range(r):
+        b = False
+        while not(b) and (x + s < c):
+            if mat[x, x + s] == 1:
+                break
+            elif mat[x, x + s] == 0:
+                for y in range(x, r):
+                    if mat[y, x + s] == 1:
+                        swap_rows(mat, y, x)
+                        b = True
+                        break
+            if not(b):
+                s = s + 1
+        for m in range(r):
+            if (x + s < c) and all((m != x, mat[m, x + s] == 1)):
+                add_row_to_row(mat, x, m)
+    pass # subroutine can't be beat
+
+def solve_augmented(C1):
+    r, c = shape(C1)
+    D = C1.copy()
+    rref(D)
+    x = 0
+
+    for a in range(r):
+        _in = True
+        for b in range(c - 1):
+            if D[a, b] != 0:
+                _in = False
+        if _in and (D[a, c] != 0):
+            raise ValueError("Inconsistent system")
+
+    ret = zeros(c - 1, dtype=int_)
+    for p in range(r):
+        if D[p, -1] == 1:
+            for n in range(c - 1):
+                if D[p, n] == 1:
+                    ret[n] = 1
+                    break
+    return ret
+
+def inv(A):
+    n, m = shape(A)
+    
+    if n != m:
+        raise ValueError("Cannot invert a matrix that isn't square.")
+    
+    if det(A) == 0:
+        raise ValueError("Cannot invert a singular matrix.")
+    
+    AB = hstack([A, identity(n, int_)])
+    rref(AB)
+
+    B = AB[:, n:]
+    return B
+
+rand = lambda shp: randint(0, 2, shp)
+
+#---------------------------------------------------------------------#
---
+++ src/stabiliser_space/stab_space.py
@@ -0,0 +1,138 @@
+import sparse_pauli as sp
+import itertools as it
+import numpy as np
+from numpy.random import randint
+from operator import or_ as union, mul
+from functools import reduce
+from . import gf2_mat as gf2
+
+__all__ = ['StabSpace', 'pauli2vec']
+
+class StabSpace(object):
+    """
+    Uses two lists of sparse_pauli.Pauli instances to represent
+    stabiliser subspaces. 
+    One list is the stabiliser generators, the other is the logical
+    generators.
+    """
+    def __init__(self, stabs, logs=None, check=True, qubits=None, n=None):
+    
+        if check:
+            # type check
+            for lst, lbl in [(stabs, 'stabiliser'), (logs, 'logical')]:
+                if any([type(p) != sp.Pauli for p in lst]):
+                    raise ValueError("all {}s must be sparse_pauli."
+                                        "Pauli instances".format(lbl))
+            # commutation check
+            for pr in it.combinations(stabs, 2):
+                if pr[0].com(pr[1]): # they anticommute
+                    raise ValueError("Input stabilisers anticommute "
+                                        ": \n{}, {}".format(*pr))
+            for pr in it.product(stabs, logs):
+                if pr[0].com(pr[1]): # they anticommute
+                    raise ValueError("Input logical anticommutes with "
+                                        "stabiliser: "
+                                        "\n{}, {}".format(*pr))
+        self.stabs = stabs
+        self.logs = logs
+    
+        if qubits is None:
+            self.qubits = sorted(list(reduce(union,
+                                [p.support() for p in stabs + logs])))
+        else:
+            self.qubits = qubits
+
+        if n is None:
+            self.n = len(self.qubits)
+        else:
+            self.n = n
+
+
+    # Gate Methods DON'T GET FANCY
+    def cnot(self, ctrl_targs):
+        for pauli in self.stabs + self.logs:
+            pauli.cnot(ctrl_targs)
+        pass
+
+    def cz(self, prs):
+        for pauli in self.stabs + self.logs:
+            pauli.cz(prs)
+        pass
+
+    def h(self, qs):
+        for pauli in self.stabs + self.logs:
+            pauli.h(qs)
+        pass
+
+    def p(self, qs):
+        for pauli in self.stabs + self.logs:
+            pauli.p(qs)
+        pass
+
+    # Prep/Measure Methods THESE DEVIATE FROM SPARSE_PAULI
+    def meas(self, op):
+        """
+        Here, we measure the eigenvalue of a new operator `op`, which
+        we hope has eigenvalue +/- 1.
+        This is Gottesman-Knill style. 
+        If the new operator is a logical, we return a random bit,
+        effectively assuming that the state is uniformly mixed over the
+        stabiliser subspace. 
+        """
+        if type(op) != sp.Pauli:
+            raise ValueError("op must be Pauli: "
+                                "{} entered.".format(op))
+
+        acom_stabs = [_ for _ in self.stabs if _.com(op)]
+        acom_logs = [_ for _ in self.logs if _.com(op)]
+        if not(acom_logs) and not(acom_stabs): #both lists empty
+            stab_mat = np.vstack(
+                [pauli2vec(s, self.qubits) for s in self.stabs]
+                ).T
+            try:
+                aug_mat = np.hstack([stab_mat, pauli2vec(op).T])
+                decomp = gf2.solve_augmented(aug_mat)
+                s = prod([a for a, b in zip(self.stabs, decomp) if b])
+                return -1 if (s * op).ph else 0 # UNSAFE
+            except ValueError: # inconsistent system, we assume
+                # stab_log_mat = np.vstack(
+                # [pauli2vec(s, self.qubits) for s in self.stabs + self.logs]
+                # ).T
+                # aug_mat = np.hstack([stab_log_mat, pauli2vec(op).T])
+                # decomp = gf2.solve_augmented(aug_mat)
+                meas_result = 2 * randint(2) - 1
+                # eliminate the proper logicals from self.logs
+                if len(self.logs) > 2:
+                    raise NotImplementedError("Logical measurements in k>1 codes not supported.")
+                self.logs = []
+                self.stabs.append(meas_result * op)
+                return meas_result
+            
+        else:
+            acom_p = (acom_stabs + acom_logs)[0]
+            for p in acom_logs:
+                self.logs.pop(p)
+                self.logs.append(acom_p * p)
+            for p in acom_stabs:
+                self.stabs.pop(p)
+                self.stabs.append(acom_p * p)
+            result = randint(2)
+            self.stabs.append(sp.Pauli(op.x_set, op.z_set, 2 * result))
+            return result 
+
+
+#------------------------convenience functions------------------------#
+
+set2vec = lambda s, bits: np.array([1 if _ in s else 0 for _ in bits])
+
+prod = lambda itrbl: reduce(mul, itrbl, sp.Pauli())
+
+def pauli2vec(pauli, qubits):
+    """
+    Takes a sparse_pauli.Pauli to a vector which is just the x and z
+    vectors stuck together. 
+    """
+    return np.hstack([set2vec(pauli.x_set, qubits),
+                        set2vec(pauli.z_set, qubits)])
+
+#---------------------------------------------------------------------#
